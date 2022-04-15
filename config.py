026751bad923e3a62828326150bfc7c8a0374a1c
Vijay Jangir <vijay.jangir@airtel.com>
2022-04-15 15:36:12 +0530

    initial commit

---
+++ config.py
@@ -0,0 +1,22 @@
+# Enter username and password of instagram account: 
+username = 'nft_art.promo'
+password = 'Insta$29'
+
+# Delay time between comments (seconds):
+delay = 20
+delay_min = 20
+delay_max = 20
+
+# Amount of comments for each tag or location:
+amount = 5
+
+# Commenting by tags - 'tag'. Commenting by locations - 'location'
+type_commenting = 'tag'
+# type_commenting = 'location'
+
+tags = ['mercedes','bmw']
+
+# locations = ['204517928/chicago-illinois', '234901722/oral-kazakhstan']
+
+# Comments (the script will take a random):
+comments = ['Hey! Nice post! @nft_art.promo', 'nice', 'Good']
